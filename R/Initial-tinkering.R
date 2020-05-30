0fe0c66625937b8356e112e959ec06bb785645c3
bmetenko <bohdanmetenko@outlook.com>
2020-05-29 22:11:01 -0400

    More style tinkering and checking how xml and list data are written inside the workbook class.

--- R/Initial-tinkering.R
+++ R/Initial-tinkering.R
@@ -72,3 +72,108 @@ write.xlsx(iris, file = "test.border.rows.new_style.xlsx", borders = "rows", hea
 
 write.xlsx(iris, file = "test.border.cols.new_style.xlsx", borders = "columns", headerStyle = new_style)
 
+write.xlsx(iris, "test.rotated.headers.xlsx", asTable = TRUE,
+           headerStyle = createStyle(textRotation = 45)) 
+# Table styling still applied first.
+## Main Educational document idea: Styling and output >>> Mardown document with images.
+
+# All list elements, read sheets, will get the style applied. Confirmed.
+
+l <- list("IRIS" = iris, "colClasses" = df)
+write.xlsx(l, file = "list.new_style.xlsx", borders = "columns", headerStyle = new_style)
+write.xlsx(l, file = "list.table.style.Light2.xlsx", asTable = TRUE, tableStyle = "TableStyleLight2")
+
+workbook_object <- write.xlsx(l, file = "list.new_style.xlsx", borders = "columns", headerStyle = new_style)
+
+str(workbook_object)
+
+# Deply nested object mapping of workbook.
+
+workbook_object
+
+object.size(workbook_object)
+# Workbook object is modified and called in place, without the need for reassignment.
+setColWidths(workbook_object, sheet = 1, cols = 1:5, widths = 20)
+
+
+# > workbook_object$colWidths
+# [[1]]
+# 1    2    3    4    5 
+# "20" "20" "20" "20" "20" 
+# attr(,"hidden")
+# [1] "0" "0" "0" "0" "0"
+# 
+# [[2]]
+# list()
+
+# write call can return a usable object variable and re-exported post processing.
+
+# saveWorkbook(wb, "writeXLSX6.xlsx", overwrite = TRUE)
+
+require(ggplot2)
+
+wb <- createWorkbook()
+
+options("openxlsx.borderColour" = "#4F80BD")
+options("openxlsx.borderStyle" = "thin")
+
+# Default style for font is assigned seperately.
+
+modifyBaseFont(wb, fontSize = 10, fontName = "Arial Narrow")
+# Modifies/appends wb$styles$fonts[[1]] with applicable xml tags list.
+
+# Add two more worksheets. Gridlines?
+addWorksheet(wb, sheetName = "Motor Trend Car Road Tests", gridLines = FALSE)
+addWorksheet(wb, sheetName = "Iris", gridLines = FALSE)
+
+
+freezePane(wb, sheet = 1, firstRow = TRUE, firstCol = TRUE) 
+## freeze first row and column
+writeDataTable(wb, sheet = 1, x = mtcars,
+               colNames = TRUE, rowNames = TRUE,
+               tableStyle = "TableStyleLight9")
+
+setColWidths(wb, sheet = 1, cols = "A", widths = 18)
+
+# Most methods can be called from instance.
+# Example wb$setColWidths(sheet = 1, cols = "A", width = 18)
+
+
+writeDataTable(wb, sheet = 2, iris, startCol = "K", startRow = 2)
+# Write data table definition is quite dense, but write data is a method that can be called with wb$method
+
+qplot(data=iris, x = Sepal.Length, y= Sepal.Width, colour = Species)
+
+# Viewport rendered plot can be attached at a specific location
+insertPlot(wb, 2, xy=c("B", 16)) ## insert plot at cell B16 in sheet 2, how is size decided? 4" x 6" default. Calls device copy, so don't change it before you do. xml based inserting of image? Stored in $drawings
+
+
+
+means <- aggregate(x = iris[,-5], by = list(iris$Species), FUN = mean)
+vars <- aggregate(x = iris[,-5], by = list(iris$Species), FUN = var)
+
+
+## write group means ### Custom styled list object.
+headSty <- createStyle(fgFill="#DCE6F1", halign="center", border = "TopBottomLeftRight")
+
+writeData(wb, 2, x = "Iris dataset group means", startCol = 2, startRow = 2)
+
+writeData(wb, 2, x = means, startCol = "B", startRow=3, borders="rows", headerStyle = headSty)
+
+## write group variances
+writeData(wb, 2, x = "Iris dataset group variances", startCol = 2, startRow = 9)
+
+writeData(wb, 2, x= vars, startCol = "B", startRow=10, borders="columns", headerStyle = headSty)
+
+setColWidths(wb, 2, cols=2:6, widths = 12) ## width is recycled for each col
+
+setColWidths(wb, 2, cols=11:15, widths = 15)
+# style mean & variance table headers
+
+# Interesting that text decoration is c()'ed together.
+s1 <- createStyle(fontSize=14, textDecoration=c("bold", "italic"))
+
+addStyle(wb, 2, style = s1, rows=c(2,9), cols=c(2,2))
+
+saveWorkbook(wb, "custom.with.plot.xlsx", overwrite = TRUE) ## save to working directory
+# Looks very good. No grid theming is interesting especially considering the way you can style the rest of the excel file. Green borders worked this time.
