eee8e257b9cd734d451fe2efc686ae134336bc6d
bmetenko <bohdanmetenko@outlook.com>
2020-06-05 15:53:59 -0400

    xlsx library function sorting

---
+++ R/Initial-tinkering-openxlsx-functions.R
@@ -0,0 +1,120 @@
+library(openxlsx)
+ls("package:openxlsx")
+
+# 69 functions contained.
+# [[ADD FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "addCreator"
+# "addFilter"
+# "addStyle"        
+# "addWorksheet"          
+
+# "cloneWorksheet"       
+
+
+# [[CONDITIONAL FORMATTING]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "conditionalFormat"    
+# "conditionalFormatting" 
+
+
+# [[CONVERSION FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "convertFromExcelRef"
+# "convertToDate"        
+# "convertToDateTime"     
+
+# [[COPY FUNCTION]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "copyWorkbook"         
+
+
+# [[CREATE FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "createComment"        
+# "createNamedRegion"
+# "createStyle"
+# "createWorkbook" 
+
+# 
+# "dataValidation" 
+# "deleteData"    
+# "freezePane"      
+
+# [[GET FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "getBaseFont"
+# "getCellRefs"
+# "getCreators"          
+# "getDateOrigin"
+# "getNamedRegions"
+# "getSheetNames"        
+# "getStyles"
+# "getTables"             
+
+
+# "insertImage"          
+# "insertPlot"      
+
+
+# "int2col"               
+# "loadWorkbook"         
+# "makeHyperlinkString" 
+# "mergeCells"     
+# "modifyBaseFont"       
+# "openXL"      
+# "pageBreak"     
+# "pageSetup"      
+
+# [[PROTECT FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+#"protectWorkbook" "protectWorksheet"      
+
+# [[READ Functions]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "read.xlsx"            
+# "readWorkbook"          
+
+# [[REMOVE FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "removeCellMerge"       
+# "removeColWidths"      
+# "removeComment"
+# "removeFilter"
+# "removeRowHeights"     
+# "removeTable"
+# "removeWorksheet"       
+
+
+# "renameWorksheet"      
+# "replaceStyle"          
+# "saveWorkbook"          
+
+# [[SET FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "setColWidths"         
+# "setFooter"  
+# "setHeader"
+# "setHeaderFooter"      
+# "setLastModifiedBy"
+# "setRowHeights"         
+
+
+# [[SHEET MANIPULATION FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "sheets"               
+# "sheetVisibility"       
+# "sheetVisibility<-"     
+# "sheetVisible"         
+# "sheetVisible<-"        
+# "showGridLines"         
+# "worksheetOrder"       
+# "worksheetOrder<-"      
+
+# [[WRITE FUNCTIONS]]
+# - # - # - # - # - # - # - # - # - # - # 
+# "write.xlsx"
+# "writeComment"
+# "writeData"
+# "writeDataTable"
+# "writeFormula" 
